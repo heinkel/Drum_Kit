d26e4694753b102503474079eb05ade63bd1dfc8
JAO Heinkel <juan.andres.otz@gmail.com>
2020-01-14 16:48:58 +0900

    Audio files into javascript objects incorporated

--- index.js
+++ index.js
@@ -2,18 +2,41 @@
 
 var keysLength = document.querySelectorAll(".drum").length;
 
+//DRUM KIT AUDIO Objects
+var snare = new Audio("sounds/snare.mp3");
+var tom1 = new Audio("sounds/tom-1.mp3");
+var tom2 = new Audio("sounds/tom-2.mp3");
+var tom3 = new Audio("sounds/tom-3.mp3");
+var tom4 = new Audio("sounds/tom-4.mp3");
+var crash = new Audio("sounds/crash.mp3");
+var kick = new Audio("sounds/kick-bass.mp3");
+
 for (var i = 0 ; i < keysLength; i++)
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-       this.style.color = "white";
+        var buttonKey = this.innerHTML;
+        switch (buttonKey){
+            case 'w' :
+                tom1.play();
+                break;
+            case 'a' :
+                tom2.play();
+                break;
+            case 's' :
+                tom3.play();
+                break;
+            case 'd' :            
+                tom4.play();
+                break;
+            case 'j' :
+                crash.play();
+                break;
+            case 'k' :
+                kick.play();
+                break;
+            case 'l' :
+                snare.play();
+                break;
+            default:
+                break;
+        }
     });
-
-
-  function Housekeeper(year,name,cleaningRepertorie) {
-      this.name = name;
-      this.yearsExp = year;
-      this.repertoire = cleaningRepertorie;
-
-  }
-
-
-  
