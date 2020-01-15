26990d59c9b6b7a2677dd26d7e58f8eda744824c
JAO Heinkel <juan.andres.otz@gmail.com>
2020-01-15 20:40:19 +0900

    almost finish button are able to be pressed now

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-var keysLength = document.querySelectorAll(".drum").length;
+
 
 //DRUM KIT AUDIO Objects
 var snare = new Audio("sounds/snare.mp3");
@@ -11,37 +11,55 @@ var tom4 = new Audio("sounds/tom-4.mp3");
 var crash = new Audio("sounds/crash.mp3");
 var kick = new Audio("sounds/kick-bass.mp3");
 
-// for (var i = 0 ; i < keysLength; i++)
-//     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-//         var buttonKey = this.innerHTML;
-document.addEventListener("keydown",function(){
-   var buttonKey = event.key;
+function playsound(key){
 
-        switch (buttonKey){
-            case 'w' :
-                tom1.play();
-                break;
-            case 'a' :
-                tom2.play();
-                break;
-            case 's' :
-                tom3.play();
-                break;
-            case 'd' :            
-                tom4.play();
-                break;
-            case 'j' :
-                crash.play();
-                break;
-            case 'k' :
-                kick.play();
-                break;
-            case 'l' :
-                snare.play();
-                break;
-            default:
-                break;
-        }
-    // });
+switch (key){
+    case 'w' :
+        tom1.play();
+        break;
+    case 'a' :
+        tom2.play();
+        break;
+    case 's' :
+        tom3.play();
+        break;
+    case 'd' :            
+        tom4.play();
+        break;
+    case 'j' :
+        crash.play();
+        break;
+    case 'k' :
+        kick.play();
+        break;
+    case 'l' :
+        snare.play();
+        break;
+    default:
+        break;
+}
+}
 
+
+// PLAY by CLICK ON BUTTONS
+var keysLength = document.querySelectorAll(".drum").length;
+for (var i = 0 ; i < keysLength; i++)
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        this.classList.add("pressed");
+        var key = this.innerHTML;
+    
+        setTimeout(function(){
+            document.getElementsByClassName(key)[0].classList.toggle("pressed");
+        },100);
+        playsound(key);
+    });
+
+// BY PRESSING KEYS
+document.addEventListener("keydown",function(event){
+   
+    document.getElementsByClassName(event.key)[0].classList.add("pressed");
+    setTimeout(function(){
+       document.getElementsByClassName(event.key)[0].classList.toggle("pressed");
+    },100);
+    playsound(event.key);
 });
